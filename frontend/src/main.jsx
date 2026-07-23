4a0677e7df7ee055a71bc360c28a39169070bd43
LeTran30 <let9888@gmail.com>
2026-07-23 14:00:16 +0700

    trigger

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
 import { store } from './store/store'
-
+//trigger deploy
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
